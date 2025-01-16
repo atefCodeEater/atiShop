fed598de63e9ae99666a75153a4fcc7c9b8d687e
atef <atefpoordehghan@gmail.com>
2025-01-16 15:20:55 +0330

    commit 7

--- app/components/related_Dashboard/arrangeAll.tsx
+++ app/components/related_Dashboard/arrangeAll.tsx
@@ -5,37 +5,61 @@ import EditProduct from "@/app/components/related_Dashboard/edit_product";
 import EditProfile from "@/app/components/related_Dashboard/edit_profile";
 
 import { Button } from "@nextui-org/react";
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function ArrangeAll({ id }: { id: string }) {
   const [item, setItem] = useState<number>(0);
+
+  function delayRender(comp: React.ReactNode) {
+    setTimeout(() => {
+      return comp;
+    }, 1000);
+  }
   return (
     <div className="h-full w-full">
       <div className="space-x-9 flex justify-center  h-14">
         <button
           onClick={(e) => setItem(4)}
-          className="rounded-md h-10 transition-all delay-150 text-sm hover:text-base
-         hover:text-[#4E0114] hover:bg-[#FFECC5] font-B_Traffic_Bold
-                 bg-[#4E0114] text-[#FFECC5] border-1
-                 border-[#FFECC5]  w-[190px] right-2"
+          className={`rounded-md h-10 
+            ${
+              item === 4
+                ? "bg-[#FFECC5] translate-y-6 shadow-sm shadow-[#000000] text-[#4E0114] text-base"
+                : "  bg-[#4E0114] text-[#FFECC5] text-sm hover:text-base"
+            } 
+          transition-all 
+          font-B_Traffic_Bold
+                  border-1
+                 border-[#FFECC5]  w-[190px] right-2`}
         >
           دیدن کالاها
         </button>
         <button
           onClick={(e) => setItem(3)}
-          className="rounded-md h-10 transition-all delay-150 text-sm hover:text-base
-         hover:text-[#4E0114] hover:bg-[#FFECC5] font-B_Traffic_Bold
-                 bg-[#4E0114] text-[#FFECC5] border-1
-                 border-[#FFECC5]  w-[190px] right-2"
+          className={`rounded-md h-10 
+            ${
+              item === 3
+                ? "bg-[#FFECC5] translate-y-6 shadow-sm shadow-[#000000] text-[#4E0114] text-base"
+                : "  bg-[#4E0114] text-[#FFECC5] text-sm hover:text-base"
+            } 
+          transition-all 
+          font-B_Traffic_Bold
+                  border-1
+                 border-[#FFECC5]  w-[190px] right-2`}
         >
           ویرایش کالا
         </button>
         <button
           onClick={(e) => setItem(2)}
-          className="rounded-md h-10 transition-all delay-150 text-sm hover:text-base
-         hover:text-[#4E0114] hover:bg-[#FFECC5] font-B_Traffic_Bold
-                 bg-[#4E0114] text-[#FFECC5] border-1
-                 border-[#FFECC5]  w-[190px] right-2"
+          className={`rounded-md h-10 
+            ${
+              item === 2
+                ? "bg-[#FFECC5] translate-y-6 shadow-sm shadow-[#000000] text-[#4E0114] text-base"
+                : "  bg-[#4E0114] text-[#FFECC5] text-sm hover:text-base"
+            } 
+          transition-all 
+          font-B_Traffic_Bold
+                  border-1
+                 border-[#FFECC5]  w-[190px] right-2`}
         >
           افزودن کالا
         </button>
@@ -44,10 +68,10 @@ export default function ArrangeAll({ id }: { id: string }) {
           className={`rounded-md h-10 
             ${
               item === 1
-                ? "bg-[#f9d691] shadow-sm shadow-[#fde1a9] text-[#4E0114] text-base"
-                : " hover:text-[#4E0114] hover:bg-[#FFECC5] bg-[#4E0114] text-[#FFECC5] text-sm hover:text-base"
+                ? "bg-[#FFECC5] translate-y-6 shadow-sm shadow-[#000000] text-[#4E0114] text-base"
+                : "  bg-[#4E0114] text-[#FFECC5] text-sm hover:text-base"
             } 
-          transition-all delay-150 
+          transition-all 
           font-B_Traffic_Bold
                   border-1
                  border-[#FFECC5]  w-[190px] right-2`}
@@ -55,12 +79,14 @@ export default function ArrangeAll({ id }: { id: string }) {
           ویرایش پروفایل
         </button>
       </div>
-      <div className="w-full h-2/3 flex justify-center-center bg-slate-400">
-        {(item === 1 && <EditProfile id={id as string} />) ||
-          (item === 2 && <AddProduct id={id as string} />) ||
-          (item === 3 && <EditProduct id={id as string} />) ||
-          (item === 4 && <ShowProducts id={id as string} />)}
-      </div>
+      {
+        <div className="w-full h-2/3 flex justify-center ">
+          {(item === 1 && <EditProfile id={id as string} />) ||
+            (item === 2 && <AddProduct id={id as string} />) ||
+            (item === 3 && <EditProduct id={id as string} />) ||
+            (item === 4 && <ShowProducts id={id as string} />)}
+        </div>
+      }
     </div>
   );
 }
--- app/components/related_Dashboard/edit_profile.tsx
+++ app/components/related_Dashboard/edit_profile.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import { useState } from "react";
+import EditUsername from "./related_EditProfile/EditUsername";
+import EditPassword from "./related_EditProfile/EditPassword";
+import EditImage from "./related_EditProfile/EditImage";
+
 export default function EditProfile_Components({ id }: { id: string }) {
-  return <div>editProfile_Components</div>;
+  return (
+    <div className="w-full flex justify-center mt-32 space-x-5">
+      <EditImage id={id} />
+      <EditPassword id={id} />
+      <EditUsername id={id} />
+    </div>
+  );
 }
